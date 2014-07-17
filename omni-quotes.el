9b2d4d49c4cea9e371d0316585e8bab46265b24e
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-17 17:10:08 +0200

    feature: colored prompt, overwrite previous oq message, and annotate for future features

--- omni-quotes.el
+++ omni-quotes.el
@@ -6,7 +6,22 @@
 ;;; §maybe: le binder avec un des trucs de quotes?
 ;;; use dash library
 
+
+;;; ¤* customs:
+;; §todo: to custom
+(defvar oq:idle-interval 5 "OmniQuote idle time, in seconds")
+(defvar oq:repeat-interval 30 "OmniQuote repeat time, in seconds")
+(defvar oq:prompt "»" "Leading prompt of messages")
+(defvar oq:color-prompt t "Is The prompt colored") ; §later:face (also for the text)
+
+(when oq:color-prompt
+  (setq oq:prompt (propertize oq:prompt 'face 'font-lock-keyword-face)))
+
 (defvar oq:quotes nil "My stupid quotes")
+;; §todo: use a ring structure randomly populated at startup
+;; §later: use category. (revision, stupid quote, emacs tips, emacs binding to learn...)
+;; §later: , offer many method to get quotes (files, web), and use a var holding
+;;        current function used to et quote. (call this several tim to populate the ring)
 (setq oq:quotes '(
 		  ;; Emacs custos
 		  "Customization is the corner stone of Emacs"
@@ -20,27 +35,29 @@
 
 		  ))
 
+;;; ¤*vars
+;; variable for the timer object
+(defvar oq:idle-timer nil "OmniQuote timer")
+
 (defun oq:random-quote ()
   "Affiche une quote au hasard"
   (interactive)
-  (message (nth (random (length oq:quotes)) oq:quotes)))
+  (message "%s %s" oq:prompt
+	   (nth (random (length oq:quotes)) oq:quotes)))
 ;; see berkeley: utilities.lisp!!!
 
 ;; Following timer recipe: (§todo:maybe extract a library?)
 
-;; variable for the timer object
-(defvar oq:idle-timer nil "OmniQuote timer")
-(defvar oq:idle-interval 5 "OmniQuote idle time, in seconds")
-(defvar oq:repeat-interval 30 "OmniQuote repeat time, in seconds")
-(defvar oq:prompt "> " "Leading prompt of messages")
-;; §todo: to custom
 
 (defun oq:idle-display-callback ()
   "OmniQuote Timer Call bac function"
   ;; ¤note: check if there is no prompt waiting!!
-  (unless (or (active-minibuffer-window) (current-message))
-    ;; §todo: bypass if oq message
-    ;; §bonux: list of message to bypass (exit, end of buffer...)
+  (unless (or (active-minibuffer-window)
+	      (and (current-message)
+		   ;; §bonux: list of message to bypass (exit, end of buffer...)
+		   ;; when s dep (s-starts-with-p oq:prompt (current-message) )
+		   (not (string-prefix-p oq:prompt (current-message)))))
+    ;; §todo: after to long idle time disable it (maybe use other timer, or number of iteration. (how to reset?))
     (oq:random-quote)))
 
 (defun oq:idle-display-start (&optional no-repeat)
