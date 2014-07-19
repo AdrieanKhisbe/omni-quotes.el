47548d9dd4df23bc23b1624064855e982c5c0b39
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-19 22:47:49 +0200

    feature: overwrite boring message (and custom to extend list)

--- omni-quotes.el
+++ omni-quotes.el
@@ -47,11 +47,35 @@
 ;; §later: , offer many method to get quotes (files, web), and use a var holding
 ;;        current function used to et quote. (call this several tim to populate the ring)
 
+(defcustom oq:boring-message-patterns
+  '(
+    "^Omni-Tags mode enabled"
+    "^Mark set"
+    "^Auto-saving...done"
+    "^Quit"
+    "End of buffer"
+    "^For information about GNU Emacs"
+    ;; yas
+    "^\\[yas\\]"
+    ;; use-package
+    "^Configuring package" "^Loading package" "^use-package idle:"
+    "^Here is not Git/Mercurial work tree"
+    ;; §maybe:  wrote, save
+    )
+  "List of message that can be overwrite by an OmniQuote"
+  :type '(repeat string) :group 'omni-quotes
+  )
+;; §todo:now regexp opt
+
 ;; §todo: (defvar current) -> structure stockant les sources courrant
 
-;;; ¤*vars
+;;; ¤>vars
 ;; variable for the timer object
 (defvar oq:idle-timer nil "OmniQuote timer")
+(defvar oq:boring-message-regexp
+  (mapconcat 'identity oq:boring-message-patterns  "\\|")
+  ;;¤if:s s-join..
+  "Regexp used to match messages that can be overwriten by a quote. Constructed from \\[oq:boring-message-patterns]")
 
 (defun oq:display-random-quote ()
   "Display a random quote obtained from \\[o:random-quote].
@@ -75,14 +99,20 @@ The quote will be prefixed by the current \\[oq:prompt]"
   ;; maybe: force? optional argument?
   ;; ¤note: check if there is no prompt waiting!!
   (unless (or (active-minibuffer-window)
-	      (and (current-message)
-		   ;; §bonux: list of message to bypass (exit, end of buffer...)
-		   ;; when s dep (s-starts-with-p oq:prompt (current-message) )
-		   ;; §todo: check if prompt not empty.
-		   ;;        extract logic in function
-		   (not (string-prefix-p oq:prompt (current-message)))))
+	      (oq:cant-redisplay))
     ;; §todo: after to long idle time disable it (maybe use other timer, or number of iteration. (how to reset?))
     (oq:display-random-quote)))
+
+(defun oq:cant-redisplay()
+  "Function that enable or not Quote to be display. (in order to avoid erasing of important messages)"
+  ;; §maybe revert logic for clarity
+  (and (current-message)
+       (let ((cm (current-message)))
+	 (not (or (string-prefix-p oq:prompt cm)
+		  ;; §todo: check if prompt not empty.
+		  ;; when s dep (s-starts-with-p oq:prompt (current-message) )
+		  (string-match oq:boring-message-regexp cm))))))
+
 
 (defun oq:idle-display-start (&optional no-repeat)
   "Add OmniQuote idle timer with repeat (by default)"
