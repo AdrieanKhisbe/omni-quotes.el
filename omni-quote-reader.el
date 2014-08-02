626848dfb924713f6741cd398c361e512239eb6a
Adriean Khisbe <adriean.khisbe@live.fr>
2014-08-02 13:32:44 +0200

    proto:reader skeleton and notes

---
+++ omni-quote-reader.el
@@ -0,0 +1,48 @@
+;;; omni-quotes-reader.el --- Utilities to read "quotes" files
+;;
+;; Copyright (C) 2014 Adrien Becchis
+;;
+;; Author: Adrien Becchis <adriean.khisbe@live.fr>
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file is used to read quotes from differents kind of files formats
+;; some custom and some not.
+
+;;; Code:
+
+;; §later: see existing format.
+
+
+;; see how
+
+;; ¤> simple file reader:
+;; format is a line a quote.
+;; ignore file w
+
+
+;; §how?
+
+;; §maybe make function to load file
+;; (no that usefull if use with-tmp...
+;; or maybe it pass the function that process the buffer!!!! (cf ruby style)
+
+
+(provide 'omni-quotes-reader)
+;;; omni-quotes-reader.el ends here
