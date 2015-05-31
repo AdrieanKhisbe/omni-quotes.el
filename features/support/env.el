b77d88ebb3f746e9dfec6ffa77c7d3a2bcc33daa
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-31 20:46:32 +0200

    test: add scaffold ecukes

---
+++ features/support/env.el
@@ -0,0 +1,32 @@
+(require 'f)
+
+(defvar omni-quotes-support-path
+  (f-dirname load-file-name))
+
+(defvar omni-quotes-features-path
+  (f-parent omni-quotes-support-path))
+
+(defvar omni-quotes-root-path
+  (f-parent omni-quotes-features-path))
+
+(add-to-list 'load-path omni-quotes-root-path)
+
+(require 'omni-quotes)
+(require 'espuds)
+(require 'ert)
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
