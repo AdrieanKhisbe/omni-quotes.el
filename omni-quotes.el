dbfbc5be6e9d86253bfbf4378337600f5cddde40
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-04 14:44:14 +0200

    build: restore batch + bump required omni-log dep

--- omni-quotes.el
+++ omni-quotes.el
@@ -6,7 +6,7 @@
 ;; Created:  2014-07-17
 ;; Version: 0.1
 ;; Keywords: convenience
-;; Package-Requires: ((dash "2.8") (omni-log "0.1.0"))
+;; Package-Requires: ((dash "2.8") (omni-log "0.1.1"))
 ;; Url: https://github.com/AdrieanKhisbe/omni-quotes.el
 
 
