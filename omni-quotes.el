1114599d9fc5d706d17d8c9e5479c8706c5aec88
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-04-16 23:56:00 +0200

    doc: swap Building notes and remove obsoletes one

--- omni-quotes.el
+++ omni-quotes.el
@@ -25,17 +25,13 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
-;; Omni Quotes is there to display quotes on you /Echo Area/ when you are idle.
-;; Whether it's funny stuff, inspirational quotes, or something you try to remember.
-
-
 ;;; Building-Notes:
-;; §draft.  idee de quote à charger.
-;; display random au fur et à mesure.  ? ring/list
-;; Utiliser aussi pour commandes à renforcer trigger.  affiche après un certain random.
 ;; §Later: plusieurs catégories.
 ;; §maybe: le binder avec un des trucs de quotes?: fortunes, and co.
+
+;;; Commentary:
+;; Omni Quotes is there to display quotes on your Echo Area when emacs is idle,
+;; whether it's funny stuff, inspirational quotes, or something you try to remember.
 
 ;;; Code:
 
